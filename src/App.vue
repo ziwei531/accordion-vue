0e44e77ec0df0336da3966e11d9851622df30a8f
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-12-20 15:50:33 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import Accordion from "./components/Accordion.vue";
+
+const itemContent = [
+    {
+        title: "Do I Have To Allow The Use Of Cookies?",
+        content:
+            "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+    },
+    {
+        title: "How Do I Change My My Page Password?",
+        content:
+            "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+    },
+    {
+        title: "What Is BankID?",
+        content:
+            "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+    },
+    {
+        title: "Whose Birth Number Can I Use?",
+        content:
+            "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+    },
+    {
+        title: "When Do I Recieve A Password Ordered By Letter?",
+        content:
+            "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan",
+    },
+];
+</script>
+
+<template>
+    <div className="container">
+        <div className="content-container">
+            <h1>Questions</h1>
+            <Accordion
+                v-for="(item, index) in itemContent"
+                :key="index"
+                :title="item.title"
+                :content="item.content"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    padding: 1.5rem;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 1.5rem;
+    font-weight: 250;
+}
+
+.content-container {
+    padding: 2rem;
+    width: 50rem;
+    background-color: white;
+    border-radius: 0.5rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { defineProps, ref } from "vue";
+
+defineProps(["title", "content"]);
+const isExpanded = ref(false);
+</script>
+
+<template>
+    <div class="accordion-container">
+        <div class="inner-accordion-container">
+            <h2>{{ title }}</h2>
+            <button @click="isExpanded = !isExpanded" class="button-container">
+                <span v-if="isExpanded">-</span>
+                <span v-else>+</span>
+            </button>
+        </div>
+        <p class="p-container" v-if="isExpanded">{{ content }}</p>
+    </div>
+</template>
+
+<style scoped>
+.accordion-container {
+    border-radius: 5px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    padding: 1rem;
+    margin-bottom: 1.2rem;
+}
+
+.inner-accordion-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.p-container {
+    padding-top: 0.5rem;
+}
+
+.button-container {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    background-color: #164e63;
+    color: white;
+}
+
+.button-container:hover {
+    cursor: pointer;
+}
+</style>
